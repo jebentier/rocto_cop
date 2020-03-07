284a8fb5dd896a6baefcf2dac0718ae7ee224f27
James Ebentier <jebentier@gmail.com>
2020-03-07 20:57:03 +0100

    Create a request helper module for the Sinatra server

---
+++ lib/rocto_cop/helpers/request.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'octokit'
+require 'openssl'
+require 'json'
+
+module RoctoCop
+  module Helpers
+    module Request
+      def raw_payload
+        @raw_payload ||= begin
+          request.body.rewind
+          request.body.read
+        end
+      end
+
+      def payload
+        @payload ||= begin
+          JSON.parse(raw_payload)
+        rescue => ex
+          fail "Unable to parse payload (#{ex}): #{raw_payload}"
+        end
+      end
+
+      def verify_webhook_signature
+        signature_header = request.env[RoctoCop::GithubApp::GITHUB_SIGNATURE_HEADER]
+        method, digest   = signature_header.split('=')
+        expected_digest  = OpenSSL::HMAC.hexdigest(method, RoctoCop::GithubApp::WEBHOOK_SECRET, raw_payload)
+
+        digest == expected_digest or halt 401
+      end
+
+      def client
+        @client ||= begin
+          installation_id    = payload.dig('installation', 'id')
+          installation_token = app_client.create_app_installation_access_token(installation_id)[:token]
+          Octokit::Client.new(bearer_token: installation_token)
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/rocto_cop/helpers/request_spec.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+require_relative '../../../spec_helper'
+
+class RequestDummyServer
+  include RoctoCop::Helpers::Request
+
+  def app_client; end
+
+  def request; end
+
+  def halt(_code); end
+end
+
+RSpec.describe RoctoCop::Helpers::Request do
+  let(:request) { double(Sinatra::Request) }
+  let(:body_proxy) { double(Rack::BodyProxy) }
+  let(:body) { { hello: "world" }.to_json }
+  let(:app_client) { double(Octokit::Client) }
+
+  subject { RequestDummyServer.new }
+
+  before(:each) {
+    allow(subject).to receive(:app_client).and_return(app_client)
+    allow(subject).to receive(:request).and_return(request)
+    allow(request).to receive(:body).and_return(body_proxy)
+    allow(body_proxy).to receive(:rewind)
+    allow(body_proxy).to receive(:read).and_return(body)
+  }
+
+  describe 'raw_payload' do
+    it "returns the unparsed json body" do
+      expect(subject.raw_payload).to eq(body)
+    end
+  end
+
+  describe 'payload' do
+    it "returns the json parsed body" do
+      expect(subject.payload).to eq(JSON.parse(body))
+    end
+  end
+
+  describe 'client' do
+    let(:body) { { installation: { id: '123123' } }.to_json }
+    let(:token) { 'thisisadummytoken' }
+
+    before(:each) do
+      expect(app_client).to receive(:create_app_installation_access_token).with('123123').and_return(token: token)
+    end
+
+    it "returns an Octokit Client" do
+      expect(subject.client).to be_a(Octokit::Client)
+    end
+  end
+
+  describe 'verify_webhook_signature' do
+    before(:each) do
+      stub_const('RoctoCop::GithubApp::WEBHOOK_SECRET', 'testdigestforverification')
+      expect(request).to receive(:env).and_return({ RoctoCop::GithubApp::GITHUB_SIGNATURE_HEADER => digest })
+    end
+
+    describe 'with matching digest' do
+      let(:digest) { "sha1=#{OpenSSL::HMAC.hexdigest("sha1", RoctoCop::GithubApp::WEBHOOK_SECRET, body)}" }
+
+      it 'it returns true' do
+        expect(subject).to_not receive(:halt)
+        expect(subject.verify_webhook_signature).to be_truthy
+      end
+    end
+
+    describe 'with mismatched digest' do
+      let(:digest) { "sha1=blahbalh" }
+
+      it 'halts with 401' do
+        expect(subject).to receive(:halt).with(401)
+        subject.verify_webhook_signature
+      end
+    end
+  end
+end
