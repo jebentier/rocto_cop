1a5bba399381527bb325f56b387061d0b4f2e631
James Ebentier <jebentier@gmail.com>
2020-03-10 09:41:05 +0100

    Update test suite to output json rich output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,7 @@ RSpec.configure do |config|
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.filter_run_when_matching :focus
-  config.example_status_persistence_file_path = 'spec/examples.txt'
+  config.example_status_persistence_file_path = 'spec/reports/last_run_status.txt'
   config.disable_monkey_patching!
   config.warnings = true
 
